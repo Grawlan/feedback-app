3a79a293b2d5c2e7b334c43e95263e458809d07b
Grawlan <izikiel@gmail.com>
2022-04-08 14:19:22 +0200

    Added functionality to send button

---
+++ src/data/FeedfbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Feedback item 1',
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: 'Feedback item 2',
+  },
+  {
+    id: 3,
+    rating: 2,
+    text: 'Feedback item 3',
+  },
+]
+
+export default FeedbackData
